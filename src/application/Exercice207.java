73aa076fcac580d340dfea78a18633637737c469
josivantarcio <josivantarcio@gmail.com>
2022-06-25 11:15:32 -0300

    feito arquivoDB.txt para receber os itens da compra

--- src/application/Exercice207.java
+++ src/application/Exercice207.java
@@ -1,82 +1,52 @@
 package application;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Exercice207 {
 
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
 
-		System.out.print("Quantidade de produtos: ");
-		int quantProd = scan.nextInt();
+		// fazer cadastrar
+		System.out.print("Quantidade de Produtos: ");
+		int quantProdutos = scan.nextInt();
 
-		System.out.print("Nome do Arquivo: ");
-		String nomeArquivo = scan.next();
-
-		for (int i = 0; i < quantProd; i++) {
-			scan.nextLine();
-			System.out.print("Digite o nome do produto: ");
-			String nome = scan.nextLine();
+		for (int i = 0; i < quantProdutos; i++) {
+			System.out.print("Produto: ");
+			String nomeProd = scan.next().toUpperCase();
 			System.out.print("Quantidade: ");
-			int q = scan.nextInt();
-			System.out.print("Valor: ");
-			double v = scan.nextDouble();
-
-			// criar arquivoOrigem
-			arquivoIn(nomeArquivo, nome, v, q);
-
-			System.out.println("produto registrado...");
+			int quantProd = scan.nextInt();
+			System.out.print("Valor:");
+			double valorProd = scan.nextDouble();
+			scan.nextLine();
+			arquivoDB(nomeProd, quantProd, valorProd);
 		}
 
-		// gerar arquivo em outra pasta e arquivo
-		System.out.print("Digite o nome da nova pasta: ");
-		String pastaDestino = scan.next();
-		criarPasta(pastaDestino);
-		arquivoOut(pastaDestino);
-		
+		// criar pasta
+		// fazer arquivo csv
+		// calcular o valor total
 
+		System.out.println("FIM");
 		scan.close();
 	}
 
-	static void arquivoIn(String nomeArquivo, String n, Double v, Integer q) {
-		String caminhoStatic = "/home/josevan/workspace/Lab-NelioAlves/ws-java/src/extras/";
-		File file = new File(nomeArquivo);
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoStatic + file, true))) {
+	// fazer arquivoDB
+	static void arquivoDB(String prod, int quant, double valor) {
 
-			bw.write(n + "," + v + "," + q);
+		String pathArquivoDB = "/home/josevan/workspace/Lab-NelioAlves/ws-java/src/extras";
+		File file = new File(pathArquivoDB);
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("/arquivoDB.txt", true))) {
+			bw.write(prod + "," + quant + "," + valor);
 			bw.newLine();
-
+		} catch (IOException e) {
+			e.printStackTrace(); 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	static void arquivoOut(String pastaDestino) {
-
-		String caminhoStatic = "/home/josevan/workspace/Lab-NelioAlves/ws-java/src/extras/" + pastaDestino + "/summary.csv";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(caminhoStatic))) {
-			String line = br.readLine();
-
-			while (line != null) {
-
-				line = br.readLine();
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	static void criarPasta(String nomePasta) {
-		String caminhoStatic = "/home/josevan/workspace/Lab-NelioAlves/ws-java/src/extras/";
-		boolean valid = new File(caminhoStatic + nomePasta).mkdir();
-	}
-
 }
