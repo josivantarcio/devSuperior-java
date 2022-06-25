d8c0c0a95d9aa4efe0ed8c597145a4d33aa9c85b
josivantarcio <josivantarcio@gmail.com>
2022-06-25 14:05:23 -0300

    exercicio207 Finalizado - criado metodo arquivoCSV e feito calculo do arquivo

--- src/application/Exercice207.java
+++ src/application/Exercice207.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -13,15 +15,14 @@ public class Exercice207 {
 		Scanner scan = new Scanner(System.in);
 
 		String pathArquivoDB = "/home/josevan/workspace/Lab-NelioAlves/ws-java/src/extras/";
+		String pathArquivoCSV = "/print/";
 
 		System.out.print("Quantidade de Produtos: ");
 		int quantProdutos = scan.nextInt();
 
 		cadastrarProd(quantProdutos, pathArquivoDB);
-		criarPasta(pathArquivoDB);
-
-		// fazer arquivo csv
-		// calcular o valor total
+		criarPasta(pathArquivoDB, pathArquivoCSV);
+		arquivoCSV(pathArquivoDB, pathArquivoCSV);
 
 		System.out.println("FIM");
 		scan.close();
@@ -59,9 +60,46 @@ public class Exercice207 {
 		}
 	}
 
+	// fazer arquivo csv
+	static void arquivoCSV(String pathArquivoDB, String pathArquivoCSV) {
+		File fileDestino = new File(pathArquivoDB + pathArquivoCSV + "summary.csv");
+		String pthOrigem = pathArquivoDB + "arquivoDB.txt";
+		File fileOrigem = new File(pthOrigem);
+
+		// ler os dados da pasta
+		try (BufferedReader br = new BufferedReader(new FileReader(fileOrigem))) {
+			// escrever e criar o arquino na pasta /OUT
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileDestino))) {
+
+				String linha = br.readLine();
+
+				while (linha != null) {
+
+					// calcular o valor total
+					String[] map = linha.split(",");
+					Double valor = Double.valueOf(map[2]);
+					Integer quantidade = Integer.valueOf(map[1]);
+					Double total = valor * quantidade;
+
+					bw.write(map[0] + " R$ " + total);
+					bw.newLine();
+					linha = br.readLine();
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	// criar pasta
-	static boolean criarPasta(String pathArquivoDB) {
-		return new File(pathArquivoDB + "/print").mkdir();
+	static boolean criarPasta(String pathArquivoDB, String pathArquivoCSV) {
+		return new File(pathArquivoDB + pathArquivoCSV).mkdir();
 	}
 
 }
